8e54a188a0693dabbce6f9ccd81c6d3b819282cf
ualioglu <u.alioglu@sabris.de>
2024-06-24 10:11:15 +0200

    Umit 24.06.24 10:10Uhr

---
+++ src/test/java/stepDefinition/GeCBewerberZurückbutton.java
@@ -0,0 +1,12 @@
+package stepDefinition;
+
+import baseClass.BaseClass;
+import pageObjekts.TS2_GL.GeC_Bewerber;
+
+public class GeCBewerberZurückbutton extends BaseClass {
+    GeC_Bewerber bewerber = new GeC_Bewerber();
+
+
+
+
+}
